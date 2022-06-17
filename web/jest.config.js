95e50ac28a88bcbda21b2c04c0854875b352fcbd
Eric Askari <eric.askari@weare.fi>
2022-06-18 01:37:57 +0300

    Renamed web

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    displayName: 'App.Web',
+    displayName: 'web',
     preset: '../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -8,7 +8,7 @@ module.exports = {
             stringifyContentPathRegex: '\\.(html|svg)$',
         },
     },
-    coverageDirectory: '../coverage/App.Web',
+    coverageDirectory: '../coverage/web',
     transform: {
         '^.+\\.(ts|js|html)$': 'jest-preset-angular',
     },
