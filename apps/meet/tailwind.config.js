724fb3918afaa73ff63bb8fa5abd2d6c531b19b3
Eric Askari <ericaskari@gmail.com>
2022-09-23 00:10:23 +0300

    Meet added

---
+++ apps/meet/tailwind.config.js
@@ -0,0 +1,10 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { join } = require('path');
+
+module.exports = {
+    mode: 'jit',
+    content: [join(__dirname, 'src/**/*.{html,ts,scss}'), ...createGlobPatternsForDependencies(__dirname)],
+    important: true,
+    darkMode: 'class',
+    theme: {}
+};
