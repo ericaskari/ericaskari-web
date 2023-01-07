2240ec9017930f32e01f846c5b4722716207ea2d
Mohammad Askari (Eric) <ericaskari@gmail.com>
2023-01-07 18:04:52 +0200

    Removed meet

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,13 +4,10 @@ import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { generatedControllers } from './generated-controllers';
 import { CoreModule } from '@ericaskari/api/core';
-import { MeetController } from './meet/meet.controller';
-import { MeetEventsGateway } from './meet/meet-events.gateway';
-import { MeetService } from './meet/meet.service';
 
 @Module({
     imports: [HttpModule, CoreModule],
-    controllers: [AppController, MeetController, ...generatedControllers],
-    providers: [MeetEventsGateway, MeetService]
+    controllers: [AppController, ...generatedControllers],
+    providers: []
 })
 export class AppModule {}
--- apps/api/src/app/meet/meet.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { BehaviorSubject } from 'rxjs';
-
-import { CallerModel, RoomModel } from '@ericaskari/shared/model';
-
-@Injectable()
-export class MeetService {
-    rooms = new BehaviorSubject<Record<string, RoomModel>>({});
-}
