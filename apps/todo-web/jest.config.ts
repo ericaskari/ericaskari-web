62be8cbd2681aa4925d348b373687e320dee28a4
ericaskari <ericaskari@gmail.com>
2023-03-23 22:44:46 +0200

    Renamed todo-web

--- apps/todo-web/jest.config.ts
+++ apps/todo-web/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-    displayName: 'todo',
+    displayName: 'todo-web',
     preset: '../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -9,7 +9,7 @@ export default {
             stringifyContentPathRegex: '\\.(html|svg)$'
         }
     },
-    coverageDirectory: '../../coverage/apps/todo',
+    coverageDirectory: '../../coverage/apps/todo-web',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
     },
